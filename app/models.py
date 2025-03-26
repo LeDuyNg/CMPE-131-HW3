3a4067e968532ed84b334765ec70a367b9eaf13f
Le Duy Nguyen <Leduy.nguyen@sjsu.edu>
2025-03-25 17:12:22 -0700

    Create the Recipe Model, make adjustment to User model to make a one-to-many relationship to Recipe, create db

--- app/models.py
+++ app/models.py
@@ -1,7 +1,24 @@
 from app import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32))
     password = db.Column(db.String(32))
     email = db.Column(db.String(32))
+    recipes = db.relationship('Recipe', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Username {}>'.format(self.username) + '<Password {}>'.format(self.password)
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    description = db.Column(db.Text, nullable = False)
+    ingredients = db.Column(db.Text, nullable = False)
+    instructions = db.Column(db.Text, nullable = False)
+    created = db.Column(db.DateTime, default=datetime.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __repr__(self):
+        return '<Recipe {}>'.format(self.title) + 'Added by {}'.format(User.query.get(self.user_id).username)
